2a6815c1c6d0df3729cd48914e384e5c7f6f8851
Alexander <int110f@gmail.com>
2021-03-31 10:44:03 +0400

    Entity

--- src/game/Entity.cpp
+++ src/game/Entity.cpp
@@ -1,6 +1,7 @@
 #include "Entity.h"
 
 Entity::Entity()
+	: rot(0.0f), pos(0.0f)
 {
 
 }
--- src/game/Entity.h
+++ src/game/Entity.h
@@ -1,11 +1,16 @@
 #ifndef ENTITY_H_
 #define ENTITY_H_
 
+#include "util/utils.h"
 
 class Entity
 {
 public:
     Entity();
+
+private:
+    vec3 pos;
+    vec3 rot;
 };
 
 #endif
