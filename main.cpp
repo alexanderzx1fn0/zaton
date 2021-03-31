6aeeee2e21eb65efd59bce12138dfe1b9e3a6b03
Alexander <int110f@gmail.com>
2021-03-31 09:20:24 +0400

    Add Sphere class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+class Sphere
+{
+public:
+    Sphere(vec3 p, float r);
+
+private:
+    vec3 position;
+    float radius;
+};
+
 int main()
 {
     return EXIT_SUCCESS;
